806755af409c95f747ec089f206ba14fa0e7bdbd
marcelo mont-morency <morencymarcelo@gmail.com>
2022-10-10 18:17:14 -0300

    Inicio do Projeto

---
+++ algoritmos em JavaScript/somatorio.js
@@ -0,0 +1,20 @@
+
+var n = prompt("informe a quantidade de numeros: ")
+        var numero;
+        var soma = 0;
+        var i = 0;
+
+        while(i < n){
+            numero = parseInt(prompt("Informe um numero: "));
+            soma = soma + numero;
+            i = i + 1;
+        }
+        document.write("A soma dos " + n + " numeros é " + soma);
+
+
+
+
+
+
+
+    
